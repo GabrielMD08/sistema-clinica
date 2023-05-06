d2d01e846cf973f81c948cc48ae0f91a2d1f321e
GabrielMD08 <aluno103.23187257gabriel@gmail.com>
2023-05-05 21:52:19 -0300

    att sistema

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 let form = document.querySelector('form');
 
 
-form.addEventListener('submit', function (event) {
+
+form.addEventListener('submit', function (event, userEmail, userPassword) {
 
     event.preventDefault();
-    let userEmail = document.getElementById('user-email').value;
-    let userPassword = document.getElementById('user-password').value;
-    switch (userEmail, userPassword){
-        case userEmail === 'nome@admdente.com.br' && userPassword === '10203040@dente':
-            window.location.assign(adm-)
+    userEmail = document.getElementById('user-email').value;
+    userPassword = document.getElementById('user-password').value;
+    switch (userEmail) {
+        case userEmail.include("@admdente.com.br"):
+            switch(userEmail){
+                case userEmail.include("gabrielm"):
+            }
+        case userEmail.include("@doctordente.com.br"):
+            switch(userEmail){
+            
+            }
+
+        case userEmail.include("@secretarydente.com.br"):
+
+        default:
+            if(userEmail === "patient@gmail.com")
+            break;
 
     }
+
 })
 
