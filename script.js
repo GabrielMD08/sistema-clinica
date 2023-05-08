12258a51ae7d373490c7020f1cbdd97b9dfeaa54
Gabriel Marques <126211645+GabrielMD08@users.noreply.github.com>
2023-05-08 07:15:13 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 let form = document.querySelector('form');
 
 
-
 form.addEventListener('submit', function (event, userEmail, userPassword) {
 
     event.preventDefault();
     userEmail = document.getElementById('user-email').value;
     userPassword = document.getElementById('user-password').value;
     switch (userEmail) {
-        case userEmail.include("@admdente.com.br"):
-            switch(userEmail){
-                case userEmail.include("gabrielm"):
-            }
-        case userEmail.include("@doctordente.com.br"):
-            switch(userEmail){
-            
-            }
-
-        case userEmail.include("@secretarydente.com.br"):
-
-        default:
-            if(userEmail === "patient@gmail.com")
+        case userEmail === "adm@dente.com.br":
+            windows.location.href = "adm-acess.html";
             break;
-
-
+        case userEmail === "doctor@dente.com.br"):
+            windows.location.href = "doctor-acess.html";
+            break;
+        case userEmail === "secretary@dente.com.br":
+            windows.location.href = "secretary-acess.html";
+            break;
+        
+        case userEmail === "patient@gmail.com":
+            windows.location.href = "adm-acess.html";
+            break;
     }
+        defaut:
+            alerta("Usuário não encontrado, tente novamente ou crie uma conta!")
 
 })
 
